eee35d7b0cadc57d10fa3a7d395cd9ce5444d61d
Rachele Edwards <dot.com1472@gmail.com>
2020-05-14 16:03:01 -0600

    finished rec-ing seed

--- data/seeds/recipe-ingredients.js
+++ data/seeds/recipe-ingredients.js
@@ -5,9 +5,23 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('recipe_ingredients').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {ingredientID: 1, recipeID: 1},
+        {ingredientID: 2, recipeID: 1},
+        {ingredientID: 3, recipeID: 1},
+        {ingredientID: 4, recipeID: 1},
+        {ingredientID: 5, recipeID: 1},
+        {ingredientID: 6, recipeID: 2},
+        {ingredientID: 7, recipeID: 2},
+        {ingredientID: 8, recipeID: 2},
+        {ingredientID: 9, recipeID: 2},
+        {ingredientID: 10, recipeID: 2},
+        {ingredientID: 11, recipeID: 2},
+        {ingredientID: 12, recipeID: 2},
+        {ingredientID: 13, recipeID: 3},
+        {ingredientID: 14, recipeID: 3},
+        {ingredientID: 15, recipeID: 3},
+        {ingredientID: 16, recipeID: 3},
+        {ingredientID: 17, recipeID: 3},
       ]);
     });
 };
