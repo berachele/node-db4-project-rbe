8c21c611e4333b69671ee1633ca32f3a20641b09
Rachele Edwards <dot.com1472@gmail.com>
2020-05-14 21:02:51 -0600

    finished mvp with model functions, did a get request to test to postman

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -1,11 +1,39 @@
 const db = require('../data/dbConfig')
 
 module.exports = {
-
+    getRecipes,
+    getShoppingList,
+    getInstructions,
 }
 
-function get() {}
+function getRecipes() {
+    return db("recipes")
+}
 
-function get() {}
+function getShoppingList(id) {
+    /*
+    select distinct RI.recipeID, r.recipe_name, i.quantity, i.measurement, i.ingredient
+    from recipe_ingredients as RI 
+    join recipes as r on R.id = RI.recipeID
+    join ingredients as i on RI.ingredientID = i.id
+    where r.id = 1;
+    */
+    return db.select("RI.recipeID", "i.quantity", "i.measurement", "i.ingredient")
+    .from("recipe_ingredients")
+    .join("recipes as r", "r.id", "=", "RI.recipeID")
+    .join("ingredients as i", "i.id", "=", "RI.ingredientID")
+    .where({recipeID: id})
+}
 
-function get() {}
+function getInstructions(id) {
+    /*
+    select distinct r.id, r.recipe_name, i.stepID, i.step
+    from recipes as r
+    join instructions as i on r.id = i.recipeID
+    where r.id = 1;
+    */
+   return db.select("r.recipe_name", "i.stepID", "i.step")
+   .from("recipes as r")
+   .join("instructions as i", "r.id", "=", "i.recipeID")
+   .where({recipeID: id})
+}
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -1,9 +1,27 @@
 const express = require('express')
 
-const Recipes = require('./recipe-model')
+const Cook = require('./recipe-model')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Cook.getRecipes()
+    .then(list => {
+        res.status(200).json(list)
+    })
+    .catch(err => {
+        console.log({err})
+        res.status(500).json({
+            message: "Something went wrong retrieving the data"
+        })
+    })
+})
 
+router.get('/', (req, res) => {
+    
+})
+router.get('/', (req, res) => {
+    
+})
 
 module.exports = router
